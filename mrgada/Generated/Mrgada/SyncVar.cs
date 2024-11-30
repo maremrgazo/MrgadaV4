143c303f74cf328cd7e9f62b7de25c928c0c03d5
maremrgazo <lazarevic.marko888@gmail.com>
2024-11-30 20:48:34 +0100

    .

--- mrgada/Generated/Mrgada/SyncVar.cs
+++ mrgada/Generated/Mrgada/SyncVar.cs
@@ -1,11 +1,23 @@
 ﻿using Serilog;
+using System.Text.Json;
+using System.Text;
+using System;
 
 public static partial class Mrgada
 {
+    public static class SyncedVars;
     public static DateTime DateTime;
     public static bool Collector1Connected;
     public static bool Collector2Connected;
     public static bool Collector3Connected;
+
+    private class SyncedVariables
+    {
+        public DateTime DateTime { get; set; }
+        public bool Collector1Connected { get; set; }
+        public bool Collector2Connected { get; set; }
+        public bool Collector3Connected { get; set; }
+    }
 
     public class SyncVarServer : MrgadaTcpServer
     {
@@ -20,7 +32,19 @@ public static partial class Mrgada
                 Thread.Sleep(1000);
 
                 DateTime = DateTime.Now;
-                Broadcast(System.Text.Encoding.UTF8.GetBytes(DateTime.ToString()));
+
+                string json = JsonSerializer.Serialize
+                (
+                    new SyncedVariables
+                    {
+                        DateTime = DateTime.Now,
+                        Collector1Connected = Collector1Connected,
+                        Collector2Connected = Collector2Connected,
+                        Collector3Connected = Collector3Connected
+                    }
+                );
+                Broadcast(Encoding.UTF8.GetBytes(json));
+
                 Log.Information($"MrgadaSyncVar Server broadcast");
             }
         }
@@ -32,8 +56,17 @@ public static partial class Mrgada
         }
         protected override void OnReceive(byte[] Buffer)
         {
-            // On Client Receive set SyncVar values
-            DateTime = DateTime.Now;
+            string json = Encoding.UTF8.GetString(Buffer);
+            var syncVars = JsonSerializer.Deserialize<SyncedVariables>(json);
+            if (syncVars != null)
+            {
+                Mrgada.DateTime = syncVars.DateTime;
+                Mrgada.Collector1Connected = syncVars.Collector1Connected;
+                Mrgada.Collector2Connected = syncVars.Collector2Connected;
+                Mrgada.Collector3Connected = syncVars.Collector3Connected;
+            }
+
+            Log.Information($"MrgadaSyncVar Client recieved broadcast");
         }
     }
 }
