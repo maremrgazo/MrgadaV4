eaa483921497e7c138a06d79507084485ddaf89f
maremrgazo <lazarevic.marko888@gmail.com>
2024-12-01 15:06:12 +0100

    .

--- mrgada/Generated/Collectors/Mrp6/Mrp6.cs
+++ mrgada/Generated/Collectors/Mrp6/Mrp6.cs
@@ -11,18 +11,12 @@ public partial class Mrgada
         public Mrp6(string collectorName, int collectorPort, string plcIp, S7.Net.CpuType cpuType, short plcRack, short plcSlot) : base(collectorName, collectorPort, plcIp, cpuType, plcRack, plcSlot)
         {
             dbDigialValves dbDigialValves = new(52, 792);
-            dbDigialValves dbDigialValves1 = new(52, 792);
-            dbDigialValves dbDigialValves2 = new(52, 792);
-            dbDigialValves dbDigialValves3 = new(52, 792);
-            dbDigialValves dbDigialValves4 = new(52, 792);
+            dbAnalogSensorsSCADA dbAnalogSensorsSCADA = new(51, 2130);
 
             AddS7db(dbDigialValves);
-            AddS7db(dbDigialValves1);
-            AddS7db(dbDigialValves2);
-            AddS7db(dbDigialValves3);
-            AddS7db(dbDigialValves4);
+            AddS7db(dbAnalogSensorsSCADA);
 
-            //for (int i = 0; i < 99; i++)
+            //for (int i = 0; i < 99; i++) // testing
             //{
             //    AddS7db(dbDigialValves);
             //}
---
+++ mrgada/Generated/Collectors/Mrp6/dbAnalogSensorsSCADA.cs
@@ -0,0 +1,21 @@
+﻿
+using static Mrgada.S7Collector;
+
+public static partial class Mrgada
+{
+    public partial class Mrp6
+    {
+        public class dbAnalogSensorsSCADA: S7db
+        {
+            public dbAnalogSensorsSCADA(int num, int len) : base(num, len)
+            {
+            }
+
+
+            public override void ParseCVs()
+            {
+                // Parse the bytes into CVs
+            }
+        }
+    }
+}
