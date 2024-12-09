896e62b0d089c6142d8e9221a59d7f19e203a4bd
maremrgazo <lazarevic.marko888@gmail.com>
2024-12-09 21:39:41 +0100

    .

--- mrgada/Generated/Historian/mrp6.cs
+++ mrgada/Generated/Historian/mrp6.cs
@@ -6,13 +6,14 @@
         public class c_mrp6 : HistorianDb
         {
             public Tag<float> PT_6B3_103;
+            public Tag<float> CT_6DR_201;
             public c_mrp6() : base("mrp6")
             {
             }
             public void Initialite()
             {
                 PT_6B3_103 = new Tag<float>(Mrp6.dbAnalogSensorsSCADA.PT_6B3_103.ValueEgu, this, "PT_6B3_103", "bar");
-                PT_6B3_103 = new Tag<float>(Mrp6.dbAnalogSensorsSCADA.CT_6DR_201.ValueEgu, this, "CT_6DR_201", "");
+                CT_6DR_201 = new Tag<float>(Mrp6.dbAnalogSensorsSCADA.CT_6DR_201.ValueEgu, this, "CT_6DR_201", "");
             }
         }
     }
